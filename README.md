bea29d7bb45fea280a702927bfef0e1e8b32ba13
kitymore <2560352426@qq.com>
2021-11-24 13:33:51 +0800

    fea/vue3+webpack5代码初始化

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue3webpack
+
+## 项目介绍
+
+- 本项目基于 webpack5 搭建 vue3 + ts 项目。
+
+## node版本
+11.6.0
+## 使用方法：
+
+```shell
+
+# 安装依赖
+npm install
+
+# 进入开发
+npm start
+
+# 打包
+npm run build
+
+# git commit
+npm run commit
+```
+
+
---
+++ src/components/Me.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    
+    <h3>Graduated from South China Agricultural University</h3>
+    <h3>Expert in javascript react vue weapp and so on!</h3>
+    <h3>The following are the details:</h3>
+   
+    <p>You have stay here for {{ count }} seconds!</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  name: 'AboutMe',
+  setup() {
+    const store = useStore()
+    const count = computed(() => store.state.count)
+    return { count }
+  },
+})
+</script>
+<style scoped lang="less">
+a {
+  color: #42b983;
+}
+
+.link a {
+  display: inline-block;
+  margin: 0 20px;
+}
+</style>
